5d43cc94ddf9f40170a2b4c232e615e2d7068b0f
Tianqi Chen <saionctq@hotmail.com>
2023-09-02 20:55:56 +1000

    Update PrintAndHide.cs

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,10 +6,11 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     int i = 3;
+    int r = -1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.r = Random.Range(150, 251);
     }
 
     // Update is called once per frame
@@ -17,5 +18,13 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log($"{this.gameObject.name}:{i}");
+        if (this.gameObject.tag == "Red" && i == 100)
+        {
+            this.gameObject.SetActive(false);
+        }
+        if (this.gameObject.tag == "Blue" && i == r)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+        }
     }
 }
